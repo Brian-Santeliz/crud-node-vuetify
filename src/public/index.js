0e89408e266dabb4e841872b21bc87926634dc1a
Brian <blsvsanteliz@gmail.com>
2021-01-25 00:10:09 -0400

    create metho post to add book. Add validation to the input form

--- src/public/index.js
+++ src/public/index.js
@@ -4,46 +4,68 @@ new Vue({
   el: "#main",
   data() {
     return {
-      articulos: [
-        {
-          id: 2,
-          descripcion: "ejemplo",
-          precio: 212,
-          stock: 232,
-        },
-        {
-          id: 3,
-          descripcion: "ejemplo",
-          precio: 212,
-          stock: 232,
-        },
-        {
-          id: 4,
-          descripcion: "ejemplo",
-          precio: 212,
-          stock: 232,
-        },
-      ],
+      valid: true,
+      libros: [],
       isModalOpen: false,
       actions: null,
+      titulo: "",
+      autor: "",
+      anio: "",
+      rules: [
+        (value) => !!value || "Campo Requerido.",
+        (value) => (value && value.length >= 3) || "Minimo 3 caracteres.",
+      ],
     };
   },
   created() {
     this.getData();
   },
   methods: {
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    },
     getData: async function () {
       try {
         const res = await fetch(endPoint);
         const data = await res.json();
-        this.articulos = data;
+        this.libros = data;
       } catch (error) {
         console.log("Ocurrio un error obteniendo la data", error);
       }
     },
     openModal: function () {
       this.isModalOpen = true;
-      this.actions = "create";
+      this.actions = 1;
+      (this.autor = ""), (this.anio = ""), (this.titulo = "");
+    },
+    closeModal: function () {
+      this.isModalOpen = false;
+      this.resetValidation();
+    },
+    actionButton: function () {
+      this.actions = 1 ? this.saveBook() : null;
+      this.isModalOpen = false;
+    },
+    saveBook: async function () {
+      const data = {
+        autor: this.autor,
+        titulo: this.titulo,
+        año: this.anio,
+      };
+      try {
+        await fetch(endPoint, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+        await this.getData();
+        this.resetValidation();
+        (this.autor = ""), (this.anio = ""), (this.titulo = "");
+      } catch (error) {
+        console.log("Ocurrio un error enviando la data", error);
+      }
     },
   },
 });
